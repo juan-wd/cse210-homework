86d44acd8187f49159295fdebd933a55a0e2ec79
JCoronel <coroneljcc@gmail.com>
2024-04-24 20:29:54 -0300

    Prep 1 excersyse completed

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -5,5 +5,12 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello and hi Prep1 World!");
+
+        Console.Write("What is your name?: ");
+        string name = Console.ReadLine();
+        Console.Write("What is your last name?: ");
+        string lastName = Console.ReadLine();
+        Console.Write("\n");
+        Console.WriteLine($"Your name is {lastName}, {name} {lastName}. ");
     }
 }
