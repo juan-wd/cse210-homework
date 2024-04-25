ce3953cad2bd473ca45871be3269a78137f04baa
JCoronel <coroneljcc@gmail.com>
2024-04-24 22:17:59 -0300

    Prep 2 excersyse completed

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,74 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Please enter your grade percentage: ");
+        string gradePercentage = Console.ReadLine();
+
+        int grade = int.Parse(gradePercentage);
+        string letter = grade.ToString();
+        
+        //if (grade >= 90)
+        //{
+        //   Console.WriteLine("Your grade is A");
+        //}
+        //else if (grade >= 80)
+        //{
+        //    Console.WriteLine("Your grade is B");
+        //}
+        //else if (grade >= 70)
+        //{
+        //    Console.WriteLine("Your grade is C");
+        //}
+        //else if (grade >= 60)
+        //{
+        //    Console.WriteLine("Your grade is D");
+        //}
+        //else if (grade < 60)
+        //{
+        //    Console.WriteLine("Your grade is F");
+        //}
+
+        //if (grade >= 70)
+        //{
+        //    Console.WriteLine("Congratulations you passed the course!!.");
+        //}
+        //else if (grade < 70)
+        //{
+        //    Console.WriteLine("You can do it better next time!!.");
+        //}
+
+
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade >= 80)
+        {
+            letter = "B";;
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";;
+        }
+        else (grade < 60)
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your grade is {letter}.");
+
+        if (grade >= 70)
+        {
+            Console.WriteLine("Congratulations you passed the course!!.");
+        }
+        else if (grade < 70)
+        {
+            Console.WriteLine("You can do it better next time!!.");
+        }
+
     }
 }
