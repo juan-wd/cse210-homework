3988d3ed6951aaffc553b7e6c9775fe936c25e2d
JCoronel <coroneljcc@gmail.com>
2024-05-04 14:02:38 -0300

    Learning activity 2 completed

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,51 +4,51 @@ using System.Transactions;
 
 class Program
 {
-    static void Main(string[] args)
-    {
-        List<int> num = new List<int>();
-        Console.Write("Please enter a list of numbers, tipe 0 when finished ");
-        int userNum = -1;
+//    static void Main(string[] args)
+//    {
+//        List<int> num = new List<int>();
+//        Console.Write("Please enter a list of numbers, tipe 0 when finished ");
+//        int userNum = -1;
 
-        while (userNum != 0)
-        {
-            Console.WriteLine("Enter your list of numbers, type 0 when finished: ");
+//        while (userNum != 0)
+//        {
+//            Console.WriteLine("Enter your list of numbers, type 0 when finished: ");
 
-            string userAnswer = Console.ReadLine();
-            userNum = int.Parse(userAnswer);
+//            string userAnswer = Console.ReadLine();
+//            userNum = int.Parse(userAnswer);
 
-            if (userNum != 0);
-            {
-                num.Add(userNum);
-            }
+//            if (userNum != 0);//
+//            {
+//                num.Add(userNum);
+//            }
 
-        }    
+//        }    
 
-        int sum = 0;
-        foreach (int number in num)
-        {
-            sum += number;
-        }
+//       int sum = 0;
+//        foreach (int number in num)
+//        {
+//            sum += number;
+//        }
 
-        Console.WriteLine("The sum is: " + sum);
+//        Console.WriteLine("The sum is: " + sum);
 
-        float average = ((float)sum) / num.Count;
-        Console.WriteLine("Te average of the list is: " + average);
+//        float average = ((float)sum) / num.Count;
+//        Console.WriteLine("Te average of the list is: " + average);
 
-        int maximum = num[0];
+//        int maximum = num[0];
 
-        foreach (int number in num)
-        {
-            if (number > maximum)
-            {
-                maximum = number;
-            }
+//        foreach (int number in num)
+//        {
+//            if (number > maximum)
+//            {
+//                maximum = number;
+//            }
 
-        }
-        Console.WriteLine($"The maximum number is: {maximum}");
+//        }
+//        Console.WriteLine($"The maximum number is: {maximum}");
         
              
 
 
-    }
+//    }
 }
--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._company = "Denso";
+        job1._jobTitle = "Account Executive";
+        job1._startYear = 2014;
+        job1._endYear = 2018;
+
+        //job1.DisplayJobDetails();
+
+        Job job2 = new Job();
+        job2._company = "Conci";
+        job2._jobTitle = "Commercial Manager";
+        job2._startYear = 2018;
+        job2._endYear = 2024;
+
+        //job2.DisplayJobDetails();
+
+
+        Resume resume1 = new Resume();
+        resume1._name = "Juan Coronel";
+
+        resume1._jobs.Add(job1);
+        resume1._jobs.Add(job2);
+
+        resume1.DisplayJobDetails();
+
     }
+    
+
 }
